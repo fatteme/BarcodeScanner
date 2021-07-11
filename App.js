9dad733d590a931834f0581acb26d5bc8b282b86
fatteme <fatt3me@gmail.com>
2021-07-11 18:25:50 +0430

    initial implementation of barcodeScanner

---
+++ App.js
@@ -0,0 +1,91 @@
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import React, { useEffect, useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`Barcode has been scanned!`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+      <View style={styles.page}>
+        <View style={styles.header}>
+          <Text style={styles.ticket}>
+            Cinema Ticket
+          </Text>
+          <Text style={styles.text}>
+                Place your ticket barcode inside the frame to scan. avoid shakes to get result quickly.
+          </Text>
+        </View>
+        <View style={styles.container}>
+          <BarCodeScanner
+            onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+            style={StyleSheet.absoluteFillObject, styles.camera}
+          />
+          {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+        </View>
+        <View style={styles.footer}>
+              
+        </View>
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({  
+  page:{
+    marginTop: 30,
+    marginBottom: 20,
+    margin: 20,
+    flex:1,
+    backgroundColor: '#ffffff',
+  },
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  camera:{
+    paddingLeft:10,
+    height:300,
+    marginBottom:10,
+    borderWidth: 5,
+    borderColor: '#0c60c190',
+    height: 300,
+    width: 300,
+  },
+  header:{
+    alignItems: 'center',
+    paddingTop: 80,
+    paddingBottom: 20,
+  },
+  ticket:{
+    fontWeight: 'bold',
+    fontSize: 23,
+    marginBottom:10,
+  },
+  text:{
+    fontSize: 16,
+    textAlign: 'center',
+    paddingLeft: 25,
+    paddingRight: 25,
+  },
+}); 
+
